66ea3211d4e749fd6ccac6ba4f8b50c7a180de2e
Adam <adam.noszczynski@gmail.com>
2020-08-25 22:44:31 +0200

    reset styles and theme colors and sizes

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,10 +1,53 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap');
+
+  *, *::after, *::before {
+    box-sizing: border-box;
+  }
+
   body {
     padding: 0;
     margin: 0;
     font-family: Poppins, serif;
+  }
+  
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: ${({ theme }) => theme.size.weight.medium};
+  }
+  
+  h1 {
+    font-size: ${({ theme }) => theme.size.font.xxl};
+  }
+  
+  h2 {
+    font-size: ${({ theme }) => theme.size.font.xl};
+  }
+  
+  h3 {
+    font-size: ${({ theme }) => theme.size.font.lg};
+  }
+  
+  h4 {
+    font-size: ${({ theme }) => theme.size.font.md};
+  }
+  
+  h5 {
+    font-size: ${({ theme }) => theme.size.font.s};
+  }
+  
+  h6 {
+    font-size: ${({ theme }) => theme.size.font.sm};
+  }
+  
+  h1, h2, h3, h4, h5, h6, p, ul, ol {
+    padding: 0;
+    margin: 0;
+  }
+  
+  ul, ol {
+    list-style: none;
   }
 `;
 
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,42 +1,49 @@
 const colors = {
-    light: '#ffffff',
-    dark: '',
-    primary: '',
-    secondary: '',
-    lightGray: '',
-    gray: '',
-    darkGray: '',
-}
+  light: '#FFFFFF',
+  dark: '#464A5F',
+  primary: '#6C63FF',
+  secondary: '#FFD037',
+  primary10: 'rgba(108, 99, 255, .1)',
+  primary30: 'rgba(108, 99, 255, .3)',
+  lightGray: '#9C9CA1',
+};
 
 const sizes = {
-    space: {
-        sm: '.5rem', // 8px
-        md: '1rem', // 16px
-        lg: '2rem', // 32px
-        xl: '4rem', // 64px
-    },
-    font: {
-        xxs: '9px',
-        xs: '12px',
-        s: '14px',
-        md: '18px',
-        lg: '24px',
-        xl: '36px',
-        xxl: '64px',
-    }
-}
+  space: {
+    sm: '.5rem', // 8px
+    md: '1rem', // 16px
+    lg: '2rem', // 32px
+    xl: '4rem', // 64px
+    xxl: '8rem', // 128px
+  },
+  font: {
+    xxs: '9px',
+    xs: '12px',
+    s: '14px',
+    md: '18px',
+    lg: '24px',
+    xl: '36px',
+    xxl: '64px',
+  },
+  weight: {
+    light: '300',
+    regular: '400',
+    medium: '500',
+    bold: '700',
+  },
+};
 
-const breakpoints = [350, 640, 768, 1024, 1280, 1440, 1600, 1920, 2560]
+const breakpoints = [350, 640, 768, 1024, 1280, 1440, 1600, 1920, 2560];
 
 const mediaQueries = breakpoints.reduce((acc, curr) => {
-    acc[curr] = `@media screen and (min-width: ${curr}px)`;
-    return acc;
+  acc[curr] = `@media screen and (min-width: ${curr}px)`;
+  return acc;
 }, {});
 
 const theme = {
-    color: colors,
-    size: sizes,
-    mq: mediaQueries,
-}
+  color: colors,
+  size: sizes,
+  mq: mediaQueries,
+};
 
 export default theme;
