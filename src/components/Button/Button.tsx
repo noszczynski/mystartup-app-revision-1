f7aea06615868302e64e7bd776aa574176d0e835
Bartłomiej Kochanowicz <bartlomiej.kochanowicz@gmail.com>
2020-09-26 14:23:27 +0200

    add extended btn component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,23 +1,60 @@
 import React, { FC } from 'react';
 import styled, { css } from 'styled-components';
 import { hot } from 'react-hot-loader/root';
+import { marginable, MarginableProps, fontable, FontableProps } from 'utils/mixins';
+import Link from '../Link/Link';
 
-const Wrapper = styled.button.attrs(({ type,size }) => ({
-  type: type || 'button',
-  size: size || 'lg',
-}))`
-  ${({ theme }) => css`
+interface Props extends MarginableProps, FontableProps {
+  btnSize?: 'sm' | 'md' | 'lg' | 'xl';
+  bgColor?:
+    | 'light'
+    | 'dark'
+    | 'white'
+    | 'black'
+    | 'blue'
+    | 'yellow'
+    | 'red'
+    | 'ocean'
+    | 'gray900'
+    | 'gray700'
+    | 'gray500'
+    | 'gray300'
+    | 'gray100'
+    | 'gray50';
+  to?: string;
+}
+
+const Wrapper = styled.button<Props>`
+  ${({ theme, btnSize = 'lg', bgColor = 'blue' }) => css`
     text-align: center;
-    background-color: ${theme.color.blue};
-    padding: ${theme.buttonSizes.lg.padding};
-    border-radius: ${theme.buttonSizes.lg.borderRadius};
+    background-color: ${theme.color[bgColor]};
+    padding: ${theme.buttonSizes[btnSize].padding};
+    border-radius: ${theme.buttonSizes[btnSize].borderRadius};
     color: ${theme.color.white};
-    font-size: ${theme.buttonSizes.lg.fontSize};
-    box-shadow: ${theme.utils.createBoxShadow([ 'minimal' ])};
-  `}`;
+    font-size: ${theme.buttonSizes[btnSize].fontSize};
+    box-shadow: ${theme.utils.createBoxShadow(['minimal'])};
+  `}
 
-const Button: FC = ({ children,...props }) => <Wrapper {...props}>{children}</Wrapper>;
+  ${marginable.css}
+  ${fontable.css}
+`;
 
-Button.displayName = "Button";
+const Button: FC<Props> = ({ to, children, ...props }) => {
+  if (to) {
+    return (
+      <Wrapper as={Link} to={to} {...props}>
+        {children}
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper to={to} {...props} type="button">
+      {children}
+    </Wrapper>
+  );
+};
+
+Button.displayName = 'Button';
 
 export default hot(Button);
